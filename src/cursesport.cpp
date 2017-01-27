2bbc1d1576c064b59e9c728d5211a32318e77717
Michael_Davies <a1studmuffin@gmail.com>
2017-01-27 15:02:10 +1000

    Added callstack dump support (DUMP_STACK() macro)
    [git-p4: depot-paths = "//depot/CDDAAndroid/src/cdda-android-project/": change = 998]

--- src/cursesport.cpp
+++ src/cursesport.cpp
@@ -747,4 +747,75 @@ int noecho()
     echoOn = 0;
     return 0;
 }
+
+#ifndef RELEASE
+
+struct android_backtrace_state
+{
+    void **current;
+    void **end;
+};
+
+_Unwind_Reason_Code android_unwind_callback(struct _Unwind_Context* context, 
+                                            void* arg)
+{
+    android_backtrace_state* state = (android_backtrace_state *)arg;
+    uintptr_t pc = _Unwind_GetIP(context);
+    if (pc) 
+    {
+        if (state->current == state->end) 
+        {
+            return _URC_END_OF_STACK;
+        } 
+        else 
+        {
+            *state->current++ = reinterpret_cast<void*>(pc);
+        }
+    }
+    return _URC_NO_REASON;
+}
+
+void dump_stack(const char* file, int line)
+{
+    LOGD("--- DUMP STACK: %s:%d ---", file, line);
+
+    const int max = 100;
+    void* buffer[max];
+
+    android_backtrace_state state;
+    state.current = buffer;
+    state.end = buffer + max;
+
+    _Unwind_Backtrace(android_unwind_callback, &state);
+
+    int count = (int)(state.current - buffer);
+
+    for (int idx = 1; idx < count; idx++) 
+    {
+        const void* addr = buffer[idx];
+        const char* symbol = "";
+
+        Dl_info info;
+        if (dladdr(addr, &info) && info.dli_sname) 
+        {
+            symbol = info.dli_sname;
+        }
+        int status = 0; 
+        char *demangled = __cxxabiv1::__cxa_demangle(symbol, 0, 0, &status); 
+
+        LOGD("\t%02d: 0x%p %s",
+                idx,
+                addr,
+                (NULL != demangled && 0 == status) ?
+                demangled : symbol);
+
+        if (NULL != demangled)
+            free(demangled);        
+    }
+
+    LOGD("---");
+}
+
+#endif // !RELEASE
+
 #endif
