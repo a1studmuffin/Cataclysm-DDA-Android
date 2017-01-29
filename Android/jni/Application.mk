88659d242c09c5784eea45f9d965204ae8d92de3
Michael_Davies <a1studmuffin@gmail.com>
2017-01-29 23:14:12 +1000

    Updated Application.mk to work better with deploy script, switched to armeabi-v7a
    [git-p4: depot-paths = "//depot/CDDAAndroid/src/cdda-android-project/": change = 1009]

--- Android/jni/Application.mk
+++ Android/jni/Application.mk
@@ -1,11 +1,19 @@
+# Reference: https://developer.android.com/ndk/guides/application_mk.html
 
-# Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 APP_STL := gnustl_static
 APP_CPPFLAGS += -std=c++11
+
+# armeabi-v7a covers 98.5%, x86 is 1.5%, armeabi is 0%
+# See http://hwstats.unity3d.com/mobile/cpu.html
 #APP_ABI := armeabi armeabi-v7a x86
-APP_ABI := armeabi
-#LOCAL_C_INCLUDES += ${ANDROID_NDK}/sources/cxx-stl/gnu-libstdc++/{NDK_TOOLCHAIN_VERSION}/include
+APP_ABI := armeabi-v7a
+
+# Do not specify APP_OPTIM here, it is done through ndk-build NDK_DEBUG=0/1 setting instead
+# See https://developer.android.com/ndk/guides/ndk-build.html#dvr
+#APP_OPTIM := debug
+#APP_OPTIM := release
+
 # Min SDK level
 APP_PLATFORM=android-10
 
