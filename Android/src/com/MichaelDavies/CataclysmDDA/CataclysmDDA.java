d8a1d74eadaf4763ebb107b85e04b7e531ae8823
Michael_Davies <a1studmuffin@gmail.com>
2017-02-06 20:41:01 +1000

    Added Android toast message for "Shortcuts enabled/disabled" ( UI feedback if no shortcuts are currently visible)
    [git-p4: depot-paths = "//depot/CDDAAndroid/src/cdda-android-project/": change = 1040]

--- Android/src/com/MichaelDavies/CataclysmDDA/CataclysmDDA.java
+++ Android/src/com/MichaelDavies/CataclysmDDA/CataclysmDDA.java
@@ -4,12 +4,29 @@ import org.libsdl.app.SDLActivity;
 import android.util.Log;
 import android.content.Context;
 import android.os.Vibrator;
+import android.widget.Toast;
 
 public class CataclysmDDA extends SDLActivity {
     private static final String TAG = "CDDA";
 
     public void vibrate(int duration) {
-        Vibrator v = (Vibrator)SDLActivity.mSingleton.getSystemService(Context.VIBRATOR_SERVICE);
-        v.vibrate(duration);
+        try {
+            Vibrator v = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+            v.vibrate(duration);
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void toast(final String message) {
+        try {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
