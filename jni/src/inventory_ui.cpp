e26e1d3b387866e34d5fc6640085c968cfa7bafd
Michael_Davies <a1studmuffin@gmail.com>
2017-02-14 13:54:25 +1000

    Pickup/drop items long number typing support
    input.h:
    - Added allow_text_entry bool to input_context.
    
    inventory_ui.cpp:
    - For multidrop selector, set allow_text_entry to true on input context.
    
    pickup.cpp:
    - For pickup selector, set allow_text_entry to true on input context.
    
    sdltiles.cpp:
    - If allow_text_entry is true on current input context, don't dismiss the keyboard after typing a key.
    - When changing input context, if we were in an allow_text_entry context, and next one isn't string input or allow_text_entry, and keyboard is still up, and we're automanaging keyboard, dismiss it.
    
    [git-p4: depot-paths = "//depot/CDDAAndroid/src/cdda-android-project/": change = 1059]

--- jni/src/inventory_ui.cpp
+++ jni/src/inventory_ui.cpp
@@ -1542,7 +1542,11 @@ void inventory_compare_selector::toggle_entry( inventory_entry *entry )
 inventory_drop_selector::inventory_drop_selector( const player &p,
         const inventory_selector_preset &preset ) :
     inventory_multiselector( p, preset, _( "ITEMS TO DROP" ) ),
-    max_chosen_count( std::numeric_limits<decltype( max_chosen_count )>::max() ) {}
+    max_chosen_count( std::numeric_limits<decltype( max_chosen_count )>::max() ) {
+#ifdef __ANDROID__
+    ctxt.allow_text_entry = true; // allow user to type a drop number without dismissing virtual keyboard after each keypress
+#endif
+    }
 
 std::list<std::pair<int, int>> inventory_drop_selector::execute()
 {
